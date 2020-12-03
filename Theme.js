1e27855faaf24aafb9599000e9394fbe1ce1878e
MathisMrvl <75432206+MathisMrvl@users.noreply.github.com>
2020-12-03 14:49:21 +0100

    Add files via upload

---
+++ Theme.js
@@ -0,0 +1,8 @@
+function foncClair() {
+	document.querySelector("#themeChange").classList.remove("noir");
+	document.querySelector("#themeChange").classList.add("blanc");
+}
+function foncNoir() {
+	document.querySelector("#themeChange").classList.remove("blanc");
+	document.querySelector("#themeChange").classList.add("noir");
+}
